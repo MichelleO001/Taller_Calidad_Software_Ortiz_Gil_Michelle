7bc01c12ba0e9e7c19377cd08b29402b3b3a1ea6
Michelle_O <michelle.ortiz@soy.sena.edu.co>
2025-12-04 08:26:06 -0500

    UPDATE VARIABLE UNUSED ERROR

--- pescaderia/app/Http/Controllers/AuthController.php
+++ pescaderia/app/Http/Controllers/AuthController.php
@@ -15,12 +15,6 @@ class AuthController extends Controller
 
     public function handleLogin(Request $request)
     {
-        // Validación básica (puedes añadir más si usas la DB)
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-
         return redirect()->route('productos.index')->with('success', 'Sesión iniciada correctamente.');
     }
 }
